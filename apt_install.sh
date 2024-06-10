989d4ae710c2fa63f66a89bb93f6de41ecd9e09c
LeonLusic <leonlusic13@gmail.com>
2024-06-10 17:22:47 +0200

    update

--- apt_install.sh
+++ apt_install.sh
@@ -1,6 +1,18 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
 
-apt install (
+# Define a list of packages
+packages=(
     tldr
     fzf
+    curl
+    git
+    btop
+    wget
+    tree
 )
+
+# Update package lists
+sudo apt update
+
+# Install packages
+sudo apt install -y "${packages[@]}"
